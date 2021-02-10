5c65bd476c6b42447d3491889daff1e2fde551b3
luccanog <luccathomaz@gmail.com>
2021-02-10 02:46:48 -0300

    fixing folder naming

---
+++ Encryption/Utils/Path.cs
@@ -0,0 +1,6 @@
+public static class Path
+{
+    public const string DECRYPTED_FILES = "Output/Decrypted";
+    public const string ENCRYPTED_FILES = "Output/Encrypted";
+
+}
--- src/utils/Path.cs
+++
@@ -1,6 +0,0 @@
-public static class Path
-{
-    public const string DECRYPTED_FILES = "output/decrypted";
-    public const string ENCRYPTED_FILES = "output/encrypted";
-
-}
