693d1145fdfea8edbe72aa466b533b66adecd644
luccanog <luccathomaz@gmail.com>
2021-02-10 01:02:42 -0300

    creating encryptor handler

---
+++ src/Encryptor.cs
@@ -0,0 +1,15 @@
+
+public class Encryptor
+{
+    IEncryptor _encryptor;
+
+    public Encryptor(IEncryptor encryptor)
+    {
+        _encryptor = encryptor;
+    }
+
+    public void Run()
+    {
+        _encryptor.Run();
+    }
+}
