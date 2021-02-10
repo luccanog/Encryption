c46e08b855704052004b7dc202a0486abd3736ca
luccanog <luccathomaz@gmail.com>
2021-02-10 04:26:52 -0300

    assymetric wip

--- Encryption/Services/Asymmetric.cs
+++ Encryption/Services/Asymmetric.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
 class Assymetric : IEncryptor
 {
     protected Assymetric()
@@ -23,6 +27,29 @@ class Assymetric : IEncryptor
 
     public void Decrypt()
     {
+        try
+        {
+            /** @TODO: KEY E IV SERÃO DESCRIPTOGRAFADOS ASSIMETRICAMENTE 
+            EM SEGUIDA, SERÃO PASSADOS COMO PARAMETRO DA FUNÇAO CREATE DECRYPTOR
+            **/
+
+            using (FileStream encryptedStream = new FileStream($"{Path.ENCRYPTED_FILES}/{OutputFile.Assymetric}.txt", FileMode.OpenOrCreate))
+            using (Aes aes = Aes.Create())
+            {
+
+                byte[] _key = new byte[5];
+                byte[] iv = new byte[5];
+
+                var aesDecryptor = aes.CreateDecryptor(_key, iv);
+
+                Core.AesDecryption(encryptedStream, aesDecryptor, OutputFile.Assymetric);
+            }
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("\nFalha ao realizar a descriptografia.");
+            throw;
+        }
 
     }
 }
