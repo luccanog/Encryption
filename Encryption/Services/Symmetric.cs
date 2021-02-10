e73c951c6f80d8e2a42357a58d1be284fe631892
luccanog <luccathomaz@gmail.com>
2021-02-10 04:26:36 -0300

    symmetric refactored

--- Encryption/Services/Symmetric.cs
+++ Encryption/Services/Symmetric.cs
@@ -31,7 +31,8 @@ class Symmetric : IEncryptor
 
         try
         {
-            using (FileStream myStream = new FileStream($"{Path.ENCRYPTED_FILES}/symmetric.txt", FileMode.OpenOrCreate))
+            string outputPath = $"{Path.ENCRYPTED_FILES}/{OutputFile.Symmetric}.txt";
+            using (FileStream myStream = new FileStream(outputPath, FileMode.OpenOrCreate))
             using (Aes aes = Aes.Create())
             {
                 aes.Key = _key;
@@ -48,6 +49,8 @@ class Symmetric : IEncryptor
                 }
             }
             Console.WriteLine("\nArquivo criptografado com sucesso!\n");
+            Console.WriteLine($"Arquivo salvo em: {outputPath}\n");
+
         }
         catch
         {
@@ -61,22 +64,15 @@ class Symmetric : IEncryptor
     {
         try
         {
-            using (FileStream encryptedStream = new FileStream($"{Path.ENCRYPTED_FILES}/symmetric.txt", FileMode.OpenOrCreate))
+            using (FileStream encryptedStream = new FileStream($"{Path.ENCRYPTED_FILES}/{OutputFile.Symmetric}.txt", FileMode.OpenOrCreate))
             using (Aes aes = Aes.Create())
             {
                 //Lê o vetor de inicializaçao do inicio do arquivo.
                 byte[] iv = new byte[aes.IV.Length];
                 encryptedStream.Read(iv, 0, iv.Length);
+                var aesDecryptor = aes.CreateDecryptor(_key, iv);
 
-                using (CryptoStream cryptStream = new CryptoStream(encryptedStream, aes.CreateDecryptor(_key, iv), CryptoStreamMode.Read))
-                using (StreamReader streamReader = new StreamReader(cryptStream))
-                {
-                    var decryptedText = streamReader.ReadToEnd();
-                    Console.WriteLine("\nArquivo descriptografado com sucesso!");
-                    Console.WriteLine($"\nMensagem criptografada originalmente: {decryptedText}");
-                    SaveDecryptedFile(decryptedText);
-                }
-
+                Core.AesDecryption(encryptedStream, aesDecryptor, OutputFile.Symmetric);
             }
         }
         catch (System.Exception)
@@ -85,14 +81,5 @@ class Symmetric : IEncryptor
             throw;
         }
 
-    }
-
-    private void SaveDecryptedFile(string text)
-    {
-        using (FileStream decryptedStream = new FileStream($"{Path.DECRYPTED_FILES}/symmetric.txt", FileMode.OpenOrCreate))
-        using (StreamWriter streamWriter = new StreamWriter(decryptedStream))
-        {
-            streamWriter.WriteLine(text);
-        }
     }
 }
