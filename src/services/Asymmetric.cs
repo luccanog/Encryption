f5a437fd259a9dcb140282aa6a54fa542bfe631f
luccanog <luccathomaz@gmail.com>
2021-02-10 01:07:03 -0300

    asymmetric structure

---
+++ src/services/Asymmetric.cs
@@ -0,0 +1,23 @@
+class Assymetric : IEncryptor
+{
+    protected Assymetric()
+    {
+
+    }
+    static Assymetric()
+    {
+        _assymetric = new Assymetric();
+    }
+    private static Assymetric _assymetric;
+    public static Assymetric Instance
+    {
+        get
+        {
+            return _assymetric;
+        }
+    }
+    public void Run()
+    {
+
+    }
+}
