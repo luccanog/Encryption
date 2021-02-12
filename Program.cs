5da351ea35c2f1811cd72b025a783e5deefcc36e
luccanog <luccathomaz@gmail.com>
2021-02-12 02:43:51 -0300

    adding start flow setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
@@ -7,12 +8,49 @@ namespace Encryption
 {
     class Program
     {
+        static IEncryptor _instance;
+
         static void Main(string[] args)
         {
+            int flow = Start();
 
-            Encryptor encryptor = new Encryptor(Symmetric.Instance);
+            switch (flow)
+            {
+                case 1: _instance = Symmetric.Instance; break;
+                case 2: _instance = Assymetric.Instance; break;
+                case 0: Environment.Exit(0); break;
+            }
+
+            Encryptor encryptor = new Encryptor(_instance);
             encryptor.Run();
 
+        }
+
+        static private int Start()
+        {
+            List<int> options = new List<int>() { 0, 1, 2 };
+            int flow = 0;
+            try
+            {
+
+
+                Console.WriteLine("Bem Vindo - Digite uma opção abaixo.\n");
+                Console.WriteLine("1 - Criptografia Simétrica\n2 - Criptografia Assimétrica\n0 - Sair");
+                flow = Convert.ToInt16(Console.ReadLine());
+
+                if (!options.Contains(flow))
+                {
+                    Console.WriteLine("\nOpção inválida, tente novamente!");
+                    Start();
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("\nOps, ocorreu um problema tente novamente!");
+                Start();
+            }
+
+            return flow;
         }
     }
 }
