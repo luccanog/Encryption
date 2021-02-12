e4e65f20e1379c25a17137458738fb5b5cc491a8
luccanog <luccathomaz@gmail.com>
2021-02-12 03:37:05 -0300

    adding new constants

---
+++ Encryption/Constants/Key.cs
@@ -0,0 +1,9 @@
+
+public static class Key
+{
+    public const string AES_KEY = "aes_key";
+    public const string IV = "iv";
+    public const string PUBLIC_KEY = "public_key";
+    public const string PRIVATE_KEY = "private_key";
+
+}
