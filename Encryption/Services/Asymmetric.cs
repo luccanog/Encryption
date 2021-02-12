97c52291d6d2c4e33cd831eeb24a8e38518a5856
luccanog <luccathomaz@gmail.com>
2021-02-12 03:44:29 -0300

    assymetric wip

--- Encryption/Services/Asymmetric.cs
+++ Encryption/Services/Asymmetric.cs
@@ -2,6 +2,15 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 
+public class RetrieveKeys
+{
+    public byte[] AesKey;
+    public byte[] PrivateKey;
+    public byte[] Iv;
+
+}
+
+
 class Assymetric : IEncryptor
 {
     protected Assymetric()
@@ -13,6 +22,16 @@ class Assymetric : IEncryptor
         _assymetric = new Assymetric();
     }
     private static Assymetric _assymetric;
+
+    private static byte[] _modulus = {214,46,220,83,160,73,40,39,201,155,19,202,3,11,191,178,56,
+                                        74,90,36,248,103,18,144,170,163,145,87,54,61,34,220,222,
+                                        207,137,149,173,14,92,120,206,222,158,28,40,24,30,16,175,
+                                        108,128,35,230,118,40,121,113,125,216,130,11,24,90,48,194,
+                                        240,105,44,76,34,57,249,228,125,80,38,9,136,29,117,207,139,
+                                        168,181,85,137,126,10,126,242,120,247,121,8,100,12,201,171,
+                                        38,226,193,180,190,117,177,87,143,242,213,11,44,180,113,93,
+                                        106,99,179,68,175,211,164,116,64,148,226,254,172,147}; // Representa uma public key gerada por terceiros.
+    private static byte[] _exponent = { 1, 0, 1 };
     public static Assymetric Instance
     {
         get
@@ -22,6 +41,43 @@ class Assymetric : IEncryptor
     }
     public void Encrypt()
     {
+        Console.WriteLine("Digite o texto a ser criptografado:");
+        string text = Console.ReadLine();
+
+        string outputPath = $"{Path.ENCRYPTED_FILES}/{OutputFile.Assymetric}.txt";
+        using (FileStream myStream = new FileStream(outputPath, FileMode.OpenOrCreate))
+        using (RSA rsa = RSA.Create(1024))
+        {
+            byte[] encryptedSymmetricKey;
+            byte[] encryptedSymmetricIV;
+
+
+            RSAParameters rsaKeyInfo = new RSAParameters();
+            /** RSA PUBLIC KEY = = modulus + exponent **/
+            rsaKeyInfo.Modulus = _modulus;
+            rsaKeyInfo.Exponent = _exponent;
+            rsa.ImportParameters(rsaKeyInfo);
+
+
+            //Create a new instance of the default Aes implementation class.  
+            Aes aes = Aes.Create();
+
+            using (CryptoStream cryptStream = new CryptoStream(myStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            using (StreamWriter streamWriter = new StreamWriter(cryptStream))
+            {
+                streamWriter.WriteLine(text);
+            }
+            //Encrypt the symmetric key and IV.  
+            encryptedSymmetricKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
+            encryptedSymmetricIV = rsa.Encrypt(aes.IV, RSAEncryptionPadding.Pkcs1);
+
+            var privateKey = rsa.ExportRSAPrivateKey();
+            File.WriteAllBytes("Foo.txt", privateKey);
+
+            SaveKeys(encryptedSymmetricKey, encryptedSymmetricIV, privateKey);
+
+        }
+
 
     }
 
@@ -29,21 +85,22 @@ class Assymetric : IEncryptor
     {
         try
         {
-            /** @TODO: KEY E IV SERÃO DESCRIPTOGRAFADOS ASSIMETRICAMENTE 
-            EM SEGUIDA, SERÃO PASSADOS COMO PARAMETRO DA FUNÇAO CREATE DECRYPTOR
-            **/
+            RSA rsa = RSA.Create();
 
-            using (FileStream encryptedStream = new FileStream($"{Path.ENCRYPTED_FILES}/{OutputFile.Assymetric}.txt", FileMode.OpenOrCreate))
-            using (Aes aes = Aes.Create())
-            {
+            // var key = rsa.Decrypt(encryptedSymmetricKey, RSAEncryptionPadding.Pkcs1);
+            // var iv = rsa.Decrypt(encryptedSymmetricIV, RSAEncryptionPadding.Pkcs1);
 
-                byte[] _key = new byte[5];
-                byte[] iv = new byte[5];
+            // using (FileStream encryptedStream = new FileStream($"{Path.ENCRYPTED_FILES}/{OutputFile.Assymetric}.txt", FileMode.OpenOrCreate))
+            // using (Aes aes = Aes.Create())
+            // {
 
-                var aesDecryptor = aes.CreateDecryptor(_key, iv);
+            //     byte[] key = new byte[5];
+            //     byte[] iv = new byte[5];
 
-                Core.AesDecryption(encryptedStream, aesDecryptor, OutputFile.Assymetric);
-            }
+            //     var aesDecryptor = aes.CreateDecryptor(key, iv);
+
+            //     Core.AesDecryption(encryptedStream, aesDecryptor, OutputFile.Assymetric);
+            // }
         }
         catch (System.Exception)
         {
@@ -52,4 +109,17 @@ class Assymetric : IEncryptor
         }
 
     }
+
+    private void SaveKeys(byte[] encryptedSymmetricKey, byte[] encryptedSymmetricIV, byte[] privateKey)
+    {
+        File.WriteAllBytes($"{Path.KEYS}/{Key.AES_KEY}.txt", encryptedSymmetricKey);
+        File.WriteAllBytes($"{Path.KEYS}/{Key.IV}.txt", encryptedSymmetricIV);
+        File.WriteAllBytes($"{Path.KEYS}/{Key.PRIVATE_KEY}.txt", privateKey);
+    }
+
+    private void RetrieveKeys()
+    {
+
+    }
+
 }
